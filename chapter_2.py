67ed7173801abba603294dce6d6ebef8da85b8fd
DESKTOP-NBK7153\admin <997675351@qq.com>
2024-07-02 18:01:57 +0800

    softmax

--- chapter_2.py
+++ chapter_2.py
@@ -3,6 +3,8 @@
 import random
 import torch
 import matplotlib.pyplot as plt
+from d2l.torch import Accumulator
+
 
 def synthetic_data(w, b, num_examples):
     """生成y=Xw+b+噪声"""
@@ -103,3 +105,139 @@ w = net[0].weight.data
 print('w的估计误差：', true_w - w.reshape(true_w.shape))
 b = net[0].bias.data
 print('b的估计误差：', true_b - b)
+
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=True)
+mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=True)
+print(len(mnist_train), len(mnist_test))
+print(mnist_train[0][0].shape)
+
+
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """绘制图像列表"""
+    figsize = (int(num_cols * scale), int(num_rows * scale))
+    _, axes = plt.subplots(nrows=num_rows, ncols=num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            ax.imshow(img)
+        else:
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+
+
+axes = show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
+# plt.imshow(axes)
+# plt.show()
+batch_size = 256
+
+# 在win中不能使用多线程，get_dataloader_workers需要使用0
+def get_dataloader_workers():
+    return 0
+
+
+train_iter = data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=0)
+
+# train_iter = iter(data.DataLoader(mnist_train, batch_size=18, shuffle=True, num_workers=4))
+# for X, y in train_iter:
+#     continue
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False, num_workers=get_dataloader_workers()))
+
+
+train_iter, test_iter = load_data_fashion_mnist(32, 28)
+for X, y in train_iter:
+    print(X.shape, X.dtype, y.shape, y.dtype)
+    break
+
+"""
+softmax
+"""
+num_inputs = 784
+num_outputs = 10
+
+W = np.random.normal(0, 0.01, (num_inputs, num_outputs))
+b = np.zeros(num_outputs)
+X = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition
+
+
+X = torch.normal(0, 1, (2, 5))
+X_prob = softmax(X)
+print(X_prob, X_prob.sum(1))
+
+def net(X):
+    # return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+    # print(X.reshape((-1, W.shape[0])).shape)
+    X = np.dot(X.reshape((-1, W.shape[0])), W) + b
+    X = torch.tensor(X)
+    # print(X.shape)
+    return softmax(X)
+
+
+y = torch.tensor([0, 2])
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+print(y_hat[[0, 1], y])
+
+print(len(y_hat))
+print(range(len(y_hat)))
+
+
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat[range(len(y_hat)), y])
+
+
+print(cross_entropy(y_hat, y))
+def accuray(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+print(accuray(y_hat, y) / len(y))
+
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  #将模型设置为评估模式
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            # print(net(X))
+            metric.add(accuray(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+X = torch.normal(0, 1, (2, 5))
+X_prob = softmax(X)
+X_prob, X_prob.sum(1)
+
+print(evaluate_accuracy(net, test_iter))
